763e725f7b531c65ff700d25387e9e6e11dbd694
mrw3w3n <68137152+mrwawanj@users.noreply.github.com>
2022-12-06 23:20:16 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -1,1 +1,41 @@
-Heckad by Mr.W4W4N
+
+
+
+<head>
+
+<link rel="icon"
+
+type="image/png" href="https://ibb.co/2F8rdgT" />
+
+  <title>Owned By Mr.R0K3S</title>
+  
+</head>
+
+<body style="background-color: rgb(0, 0, 0); color: rgb(0, 0, 0);"
+
+ alink="#ee0000" link="#0000ee" vlink="#551a8b">
+
+<table width=100% height=100%><td align=center>
+
+<span style="color: rgb(127, 127, 126);"><br>
+
+<span style="font-family: monospace;">
+
+ <img src="https://i.ibb.co/VpfgK4Q/16635099f95-picsay.jpg" width="45%"><br>
+<br>
+	<br>
+		
+ <span
+
+style="color: rgb(253, 253, 251);">Owned By Mr.R0K3S<br><br>#TEAM ROKES 315<br><br>Message<br><br>Ternyata memang benar, sila kelima adalah keadilan bagi para penguasa<br>dari semua kasus kasus ini, memang tidak ada yang bisa di percaya lagi<br>dari pemerintahan dan aparat negara,, Ya allah tunjukan lah keadilan mu<br>di depan bajingan bajingan ini kami sebagai rakyat kecil<br>sudah berusaha semampu kami walaupun berakhir dengan sia sia<br><font color="White"><font <br><br><br><font face="iceland" color="white" size="3">We Are: [ TEAM ROKES 315 - Foursdeath Team - Hacktivist Of Garuda ]<br><b></b><p><font color="yellow" face="times"><br><b>INDONESIAN DEFACER 2018</b></font><br><br> 
+              <br></span></span></span><br>
+
+
+<br>
+	<br>
+<center><audio autoplay="autoplay" controls="controls" src="https://fitapess.com/.quarantine/j.mp3" type="audio/mpeg"/></center>
+
+                       </audio>
+
+</html>
+
