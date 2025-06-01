60e3730e545e6ef3a5d24aabf3500aa4dc081b8d
mrw3w3n <68137152+mrwawanj@users.noreply.github.com>
2025-06-02 05:21:52 +0700

    Update and rename find3.php to find4.php

--- find4.php
+++ find4.php
@@ -1,6 +1,6 @@
 <?php
 // URL tetap (bisa kamu ganti ke URL lain)
-$url = 'https://paste.ee/r/AbrRPaML/0';
+$url = 'https://paste.ee/r/q7BMl7RL/0';
 
 $keywords_raw = @file_get_contents($url);
 if (!$keywords_raw) {
